0485f2d76f051a4f140595bc78022898c9120dd2
songshs <1576575650@qq.com>
2020-12-19 16:13:21 +0800

    email2

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,13 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::group(['middleware'=>'auth'],function(){
     //邮箱验证提示路由
     Route::get('/email_verify_notice','PagesController@emailVerifyNotice')->name('email_verify_notice');
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
+    Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
     //
     Route::group(['middleware'=>'email_verified'],function (){
         Route::get('/test',function (){
             return "Your email is verified";
         });
     });
+
 });
